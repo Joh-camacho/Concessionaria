57f56331b965651cc2acb59d2d4bd44a767c6090
Johnny Camacho <joh_camacho@outlook.com>
2022-10-04 20:20:57 -0300

    feat: apply mask license plate input.

--- js/scripts.js
+++ js/scripts.js
@@ -9,31 +9,42 @@ function applyMask(input, regex) {
     }, 1);
 }
 
-function regexPhone(number) {
-    number = number.replace(/\D/g, "");
-    number = number.replace(/^(\d{2})(\d)/g, "($1) $2"); 
-    number = number.replace(/(\d)(\d{4})$/, "$1-$2");
+function regeBoard(board) {
+    // /^[A-Z]{3}[0-9]{4}$/
+    // /^[a-zA-Z]{3}[0-9][A-Za-z0-9][0-9]{2}$/
 
-    return number;
+    return board.replace(/^([a-zA-Z]{3})([0-9][A-Za-z0-9][0-9]{2})$/, "$1-$2")
 }
 
 function regexCPF(cpf) {
-    cpf = cpf.replace(/\D/g, "")
-    cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2") 
-    cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2")
-    cpf = cpf.replace(/(\d{3})(\d{1,2})$/, "$1-$2")
-
     return cpf
+        .replace(/\D/g, "")
+        .replace(/(\d{3})(\d)/, "$1.$2")
+        .replace(/(\d{3})(\d)/, "$1.$2")
+        .replace(/(\d{3})(\d{1,2})$/, "$1-$2");
+}
+
+function regexPhone(number) {
+    return number
+        .replace(/\D/g, "")
+        .replace(/^(\d{2})(\d)/, "($1) $2")
+        .replace(/(\d)(\d{4})$/, "$1-$2");
 }
 
 window.onload = function () {
-    const tel = document.getElementById('inputPhone')
+    const board = document.getElementById('inputBoardCar')
     const cpf = document.getElementById('inputCPF')
+    const tel = document.getElementById('inputPhone')
 
-    tel.onkeyup = function () {
-        applyMask(this, regexPhone);
+    board.onkeyup = function () {
+        applyMask(this, regeBoard);
     }
     cpf.onkeyup = function () {
         applyMask(this, regexCPF);
     }
+    tel.onkeyup = function () {
+        applyMask(this, regexPhone);
+    }
 }
+
+// https://www.youtube.com/watch?v=3Ec9zY1C2og
